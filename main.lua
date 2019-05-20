c71643cc4ccc07d2a39a56b0a20ee2997d6ecbd1
1338 <maxvandersluis@yahoo.com>
2019-05-20 15:49:25 +0200

    Restructuring the main guildsystem and adding LDOC

--- main.lua
+++ main.lua
@@ -1,48 +1,63 @@
-guildsystem = {
-	core = {
-		optionsFile = "/custom/guildsystem/options.json",
-		load = {},
-		save = {}
-	}
-}
+-------
+-- guildsystem
+-- @module guildsystem
+guildsystem = {}
+
+--- options file
+guildsystem.optionsFile = "/custom/guildsystem/options.json"
+--- module version: 0.1
+guildsystem.version = '0.1'
+
+--- Load functions
+-- @section load
 
 --- Loads options
 -- loads options from file, returns true if loaded, false if not
+-- @see guildsystem.optionsFile
 -- @return boolean
-function guildsystem.core.load.options()
-	guildsystem.options = jsonInterface.load(guildsystem.core.optionsFile)
+function guildsystem.loadOptions()
+	guildsystem.options = jsonInterface.load(guildsystem.optionsFile)
 	return (guildsystem.options ~= nil)
-end
-
---- Save options
--- saves options to file, returns true if loaded, false if not. (needed to allow server owner edit options on the fly
--- @return boolean
-function guildsystem.core.save.options()
-	return jsonInterface.save(guildsystem.core.optionsFile, guildsystem.options)
 end
 
 --- Loads guilds
 -- loads guilds from file, returns true if loaded, false if not
 -- @return boolean
-function guildsystem.core.load.guilds()
+function guildsystem.loadGuilds()
 	guildsystem.guilds = jsonInterface.load(guildsystem.options.files.guilds)
 	return (guildsystem.guilds ~= nil)
+end
+
+--- Loads submodules
+-- Load submodules from guildsystem.options.submodules
+function guildsystem.loadSubmodules()
+    if guildsystem.submodules == nil then
+        guildsystem.submodules = {}
+    end
+	for k, v in ipairs(guildsystem.options.submodules) do
+		guildsystem.submodules[v] = require("../" .. v .. "/main")
+	end
+end
+
+--- Save functions
+-- @section save
+
+--- Save options
+-- saves options to file, returns true if loaded, false if not. (needed to allow server owner edit options on the fly
+-- @return boolean
+function guildsystem.saveOptions()
+	return jsonInterface.save(guildsystem.optionsFile, guildsystem.options)
 end
 
 --- Saves guilds
 -- saves guilds to file, returns true if saved, false if not
 -- @return boolean
-function guildsystem.core.save.guilds()
+function guildsystem.saveGuilds()
 	return jsonInterface.save(guildsystem.options.files.guilds, guildsystem.guilds)
 end
 
---- Loads submodules
--- Load submodules from guildsystem.options.submodules
-function guildsystem.load.submodules()
-	for k, v in ipairs(guildsystem.options.submodules) do
-		guildsystem.submodules[v] = require("../" .. v .. "/main")
-	end
-end
+--- Main method
+-- @section main
 
 --- Init function
 -- Starts the guild system and loads needed files for core
@@ -60,19 +75,6 @@ function guildsystem.core.init()
 	end
 end
 
---- filecheck
--- check if file exists, returns boolean
--- @string path
--- @return boolean
-function guildsystem.core.fileCheck(path)
-	local fh = io.open( path, "r" )
-	if fh then
-		io.close(fh)
-		return true
-	end
-	return false
-end
-
-customEventHooks.registerHandler("OnServerPostInit", guildsystem.core.init)
+customEventHooks.registerHandler("OnServerPostInit", guildsystem.init)
 
 return guildsystem
